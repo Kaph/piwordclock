2d6cdae884de684a3869c7a3261b491eac9d3d39
kp <kurtispoole@gmail.com>
2016-04-09 00:47:00 +0100

    Fixed bug where 'to' midnight wouldn't display the hour.

--- wordclock.js
+++ wordclock.js
@@ -79,7 +79,7 @@ function parseTime(currentTime) {
   }
 
   // We need to parse the hour too. Tedious.
-  if (hour == 0 || hour == 12) {
+  if (hour == 0 || hour == 12 || hour == 24) {
       currentWords.push(hoursLeds[11])
   } else if (hour == 1 || hour == 13) {
       currentWords.push(hoursLeds[0])
